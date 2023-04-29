e1496025c21d880f2ef793563156bd9914207d83
Cezary Stachurski <czareks20006@gmail.com>
2023-04-29 21:33:48 +0200

    add button component

--- goals-progress/src/App.js
+++ goals-progress/src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
+import { FaPlus } from 'react-icons/fa'
 
 import Goals from './components/Goals';
+import Button from './components/Button';
 
 function App() {
   return (
+    <>
     <div className="container">
       <div className="content">
         <Goals/>
       </div>
+      
     </div>
+    <div className="my-1 text-center">
+      <Button text={
+        <>
+          <FaPlus style={{ verticalAlign: 'middle' }}/> NEW GOAL
+        </>
+      } color={'#39a0ca'}/>
+    </div>
+  </>
   );
 }
 
---
+++ goals-progress/src/components/Button.js
@@ -0,0 +1,12 @@
+const Button = ({ text, color }) => {
+  return (
+    <button 
+        className="btn"
+        style={{backgroundColor: color}} 
+    >
+    {text}
+    </button>
+  )
+}
+
+export default Button
