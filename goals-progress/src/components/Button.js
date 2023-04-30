c1518fee5d27d9343cbfc152a1003c8bdd4d5c54
Cezary Stachurski <czareks20006@gmail.com>
2023-04-30 13:11:18 +0200

    add onClick button prop

--- goals-progress/src/components/Button.js
+++ goals-progress/src/components/Button.js
@@ -1,12 +1,21 @@
-const Button = ({ text, color }) => {
+import PropTypes from 'prop-types'
+
+const Button = ({ text, color, onClick }) => {
   return (
     <button 
         className="btn"
-        style={{backgroundColor: color}} 
+        style={{backgroundColor: color}}
+        onClick={onClick} 
     >
     {text}
     </button>
   )
+}
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func
 }
 
 export default Button
