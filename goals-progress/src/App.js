0be79dd7ab64dd9b64892f8337c4800ac170af3d
Cezary Stachurski <czareks20006@gmail.com>
2023-05-03 19:40:33 +0200

    moved goals array to app.js

--- goals-progress/src/App.js
+++ goals-progress/src/App.js
@@ -7,15 +7,59 @@ import Footer from './components/Footer';
 import GoalCreateForm from './components/GoalCreateForm';
 
 function App() {
+  const goals = [
+    {
+      id: 1,
+      name: 'Filmy',
+      description: 'Obejrzeć 20 filmów',
+      currentValue: 4,
+      targetValue: 20,
+      progress: 20,
+      daysLeft: 230
+    },
+    {
+      id: 2,
+      name: 'Przepisy',
+      description: 'Wypróbować 10 nowych przepisów',
+      currentValue: 5,
+      targetValue: 10,
+      progress: 50,
+      daysLeft: 230
+    },
+    {
+      id: 3,
+      name: 'Książki',
+      description: 'Przeczytać 10 książek',
+      currentValue: 3,
+      targetValue: 10,
+      progress: 30,
+      daysLeft: 230
+    },
+    {
+      id: 4,
+      name: 'Badanie krwi',
+      description: 'Zrobić morfologie krwi 2 razy',
+      currentValue: 1,
+      targetValue: 2,
+      progress: 50,
+      daysLeft: 230
+    }
+  ]
+
+  // add goal
+  const addGoal = (goal) => {
+    console.log(goal);
+  }
+
   return (
     <BrowserRouter>
       <div className="wrapper">
         <Header appName='GoalsProgress'/>
         <Routes>
-          <Route path='/' element={<Goals/>}/>
+          <Route path='/' element={<Goals goals={goals}/>}/>
+          <Route path='/goals' element={<Goals goals={goals}/>}/>
           <Route path='/goal/id' element={<>Goal Details</>}/>
-          <Route path='/goals' element={<Goals/>}/>
-          <Route path='/goals/create' element={<GoalCreateForm/>}/>
+          <Route path='/goals/create' element={<GoalCreateForm onAdd={addGoal}/>}/>
         </Routes>
       </div>
       <Footer/>
--- goals-progress/src/components/Goals.js
+++ goals-progress/src/components/Goals.js
@@ -4,46 +4,7 @@ import { Link } from 'react-router-dom'
 import Goal from "./Goal"
 import Button from './Button'
 
-const Goals = () => {
-  const goals = [
-    {
-      id: 1,
-      name: 'Filmy',
-      description: 'Obejrzeć 20 filmów',
-      currentValue: 4,
-      targetValue: 20,
-      progress: 20,
-      daysLeft: 230
-    },
-    {
-      id: 2,
-      name: 'Przepisy',
-      description: 'Wypróbować 10 nowych przepisów',
-      currentValue: 5,
-      targetValue: 10,
-      progress: 50,
-      daysLeft: 230
-    },
-    {
-      id: 3,
-      name: 'Książki',
-      description: 'Przeczytać 10 książek',
-      currentValue: 3,
-      targetValue: 10,
-      progress: 30,
-      daysLeft: 230
-    },
-    {
-      id: 4,
-      name: 'Badanie krwi',
-      description: 'Zrobić morfologie krwi 2 razy',
-      currentValue: 1,
-      targetValue: 2,
-      progress: 50,
-      daysLeft: 230
-    }
-  ]
-
+const Goals = ({ goals }) => {
   return (
     <>
       <div className="goals container shadow">
