fec2bbb96cc5882949d9fc46c624478fb6a8fd81
Cezary Stachurski <czareks20006@gmail.com>
2023-05-04 13:34:26 +0200

    create actions component

---
+++ goals-progress/src/components/Actions.js
@@ -0,0 +1,21 @@
+const Actions = () => {
+  
+    return (
+        <div className="actions text-center">
+            <div className="action">
+                Progress
+            </div>
+            <div className="action">
+                Edit
+            </div>
+            <div className="action">
+                Delete
+            </div>
+            <div className="action">
+                Archive
+            </div>
+        </div>
+  )
+}
+
+export default Actions
