f2b57fe8c369238e91154ede34897729c835717f
Cezary Stachurski <czareks20006@gmail.com>
2023-08-30 14:05:49 +0200

    Migrating to Vite.js from create-react-app

--- goals-progress/src/app/api/agent.ts
+++ goals-progress/src/app/api/agent.ts
@@ -13,10 +13,10 @@ const sleep = (delay: number) => {
     })
 }
 
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 axios.interceptors.response.use(async response => {
-    await sleep(500);
+    if (import.meta.env.DEV) await sleep(500);
     return response;
 }, (error: AxiosError) => {
     const {data, status, config} = error.response as AxiosResponse;
--- goals-progress/src/index.tsx
+++ goals-progress/src/index.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
 import 'react-datepicker/dist/react-datepicker.css'
 import { StoreContext, store } from './app/stores/store';
 import { RouterProvider } from 'react-router-dom';
@@ -13,8 +12,3 @@ root.render(
       <RouterProvider router={router}/>
   </StoreContext.Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
