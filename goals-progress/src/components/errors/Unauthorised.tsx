e48adda75e0d9d79d31982a91424843d8b42db2f
Cezary Stachurski <czareks20006@gmail.com>
2023-09-30 15:16:24 +0200

    Display link to login page when unauthorised

--- goals-progress/src/components/errors/Unauthorised.tsx
+++ goals-progress/src/components/errors/Unauthorised.tsx
@@ -1,6 +1,11 @@
 import { FaUserLock } from "react-icons/fa"
+import Button from "../common/Button"
+import { Link } from "react-router-dom"
+import { useStore } from "../../app/stores/store";
 
 const Unauthorised = () => {
+  const {userStore} = useStore();
+
   return (
     <>
         <div className="errors container shadow">
@@ -9,6 +14,11 @@ const Unauthorised = () => {
             </h1>
             <h1 className="text-center">Unauthorised</h1>
             
+            <p className="text-center">
+              <Link to='/'>
+                <Button text="Sign in" onClick={() => userStore.logout()}/>
+              </Link>
+            </p>
         </div>
     </>
   )
