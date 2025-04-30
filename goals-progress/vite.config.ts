30083cb4e021ad0ffc551dc021b9187be40fd979
Cezary Stachurski <cstachurski@lbpro.pl>
2025-04-30 15:10:02 +0200

    Add PWA

--- goals-progress/vite.config.ts
+++ goals-progress/vite.config.ts
@@ -1,5 +1,6 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(() => {
     return {
@@ -9,6 +10,30 @@ export default defineConfig(() => {
         server: {
             port: 3000
         },
-        plugins: [react()]
+        plugins: [react(),
+            VitePWA({
+                registerType: 'autoUpdate',
+                manifest: {
+                  name: 'GoalsProgress',
+                  short_name: 'GoalsProgress',
+                  start_url: '/goals',
+                  display: 'standalone',
+                  theme_color: '#161748',
+                  background_color: '#161748',
+                  icons: [
+                    {
+                      src: 'icons/icon-128x128.png',
+                      sizes: '128x128',
+                      type: 'image/png',
+                    },
+                    {
+                      src: 'icons/icon-256x256.png',
+                      sizes: '256x256',
+                      type: 'image/png',
+                    }
+                  ]
+                }
+              })
+        ]
     }
 })
